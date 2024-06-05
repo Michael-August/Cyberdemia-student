adf3d3f4984340d30e2c2e6a1d550b6f47033c65
Michael-August <michaelfrancis135@gmail.com>
2024-06-05 01:46:17 +0100

    node module installation for setup

--- src/lib/utils/cn.ts
+++ src/lib/utils/cn.ts
@@ -1,6 +1,7 @@
-import clsx, { ClassValue } from 'clsx'
+import clsx from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const cn = (...args: any) => {
     return twMerge(clsx(args))
 }
