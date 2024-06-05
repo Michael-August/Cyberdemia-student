8929ab77880b706016000810d898c65fa7f530a7
Michael-August <michaelfrancis135@gmail.com>
2024-06-05 22:22:23 +0100

    after pull for layout update - and extra placeholder files for other modules

--- src/components/custom-hooks/useInnerLayout.tsx
+++ src/components/custom-hooks/useInnerLayout.tsx
@@ -1,19 +1,19 @@
-"use client";
-import { useLayoutContext } from "../../../context/LayoutContext";
-import { Navbar } from "./useNavbar";
-import { Sidebar } from "./useSidebar";
+'use client';
+import { useLayoutContext } from '../../../context/LayoutContext';
+import { Navbar } from './useNavbar';
+import { Sidebar } from './useSidebar';
 
 export function RootLayoutInner({ children }: any) {
   const { state } = useLayoutContext();
 
-  let dynamicClass = "";
+  let dynamicClass = '';
 
-  if (state.navbarType !== "none") {
-    dynamicClass += "md:mt-[30px]";
+  if (state.navbarType !== 'none') {
+    dynamicClass += 'md:mt-[30px]';
   }
 
-  if (state.sidebarType !== "none") {
-    dynamicClass += " md:ml-[250px] md:mr-[30px]";
+  if (state.sidebarType !== 'none') {
+    dynamicClass += ' md:ml-[250px] md:mr-[30px]';
   }
 
   return (
