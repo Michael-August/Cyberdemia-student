b3aa4c9d45b7f3a326cf985edc30e43297e02672
omenvc <chimavalentine11@gmail.com>
2024-06-08 08:44:52 +0100

    resolve errors

--- src/app/(dashboard)/home/page.tsx
+++ src/app/(dashboard)/home/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-
-import CustomCourse from '@/components/home/CustomCourse';
-import RecomendedCourse from '@/components/home/RecomendedCourse';
-import ResumeLearning from '@/components/home/ResumeLearning';
+"use client";
+import React from "react";
+import ResumeLearning from "@/components/home/ResumeLearning";
+import CustomCourse from "@/components/home/CustomCourse";
+import RecomendedCourse from "@/components/home/RecomendedCourse";
+import { useRouter } from "next/navigation";
 const Home = () => {
+  const router = useRouter();
   return (
     <div className="flex flex-col gap-8  h-[100%] px-4 md:px-0">
       <div className="flex flex-col gap-2 ">
@@ -14,9 +16,9 @@ const Home = () => {
           <div className="w-[103px] h-[8px] bg-[#AC1D7E]"></div>
           <hr
             style={{
-              height: '1px',
-              backgroundColor: '#AC1D7E',
-              border: 'none',
+              height: "1px",
+              backgroundColor: "#AC1D7E",
+              border: "none",
             }}
           />
         </div>
@@ -24,7 +26,7 @@ const Home = () => {
 
       <ResumeLearning />
       <hr
-        style={{ height: '1px', backgroundColor: '#AC1D7E', border: 'none' }}
+        style={{ height: "1px", backgroundColor: "#AC1D7E", border: "none" }}
       />
       <span className="text-[16px] font-extrabold">
         Request for custom course
@@ -33,7 +35,24 @@ const Home = () => {
       <span className="text-[16px] font-extrabold">
         Top Recommended Courses
       </span>
-      <RecomendedCourse />
+      <div className="flex  gap-6">
+        <RecomendedCourse
+          availableCourses="7 Courses Available"
+          image={"/images/card1.svg"}
+          title="Cyber Security Technical Training"
+          body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
+          handleClick={() => router.push("home/dhjfhjdfh")}
+          isCourseAvailable={true}
+        />
+        <RecomendedCourse
+          availableCourses="3 Courses Available"
+          image={"/images/card2.svg"}
+          title="Cyber Security Awareness "
+          body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
+          handleClick={() => router.push("home/shfdhf")}
+          isCourseAvailable={true}
+        />
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,17 @@
-'use client';
-import React, { useEffect } from 'react';
+"use client";
+import React, { useEffect } from "react";
 
-import { useLayoutContext } from '../../context/LayoutContext';
+import { useLayoutContext } from "../../context/LayoutContext";
 
 const Home = () => {
   const { dispatch } = useLayoutContext();
   useEffect(() => {
-    dispatch({ type: 'SET_NAVBAR', navbarType: 'webNavbar' });
-    dispatch({ type: 'SET_SIDEBAR', sidebarType: 'none' });
+    dispatch({ type: "SET_NAVBAR", navbarType: "webNavbar" });
+    dispatch({ type: "SET_SIDEBAR", sidebarType: "none" });
 
     return () => {
-      dispatch({ type: 'SET_NAVBAR', navbarType: 'none' });
-      dispatch({ type: 'SET_SIDEBAR', sidebarType: 'none' });
+      dispatch({ type: "SET_NAVBAR", navbarType: "none" });
+      dispatch({ type: "SET_SIDEBAR", sidebarType: "none" });
     };
   }, [dispatch]);
 
