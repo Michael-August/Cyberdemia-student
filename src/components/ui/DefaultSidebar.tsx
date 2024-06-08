17b03235e454cb994247e53e2fdee8952bbb9e05
chisom chima <chimachisom360@gmail.com>
2024-06-08 10:57:18 +0100

    change routes

--- src/components/ui/DefaultSidebar.tsx
+++ src/components/ui/DefaultSidebar.tsx
@@ -24,29 +24,28 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, onClose }) => {
   const isSmallScreen = useMediaQuery('(max-width:768px)');
 
   const sidebarLinks = [
-    { img: PiHouseFill, link: '/home', text: 'Home' },
-    { img: GoBook, link: '/myCourses', text: 'My Courses' },
+    { img: PiHouseFill, link: '/student/home', text: 'Home' },
+    { img: GoBook, link: '/student/courses', text: 'My Courses' },
     {
       img: isSmallScreen ? SiSimpleanalytics : PiCertificateBold,
-      link: '/certificate',
-      text: isSmallScreen ? 'Analytics' : 'Certificates',
+      link: '/student/certificate',
+      text: isSmallScreen ? 'Certificate' : 'Certificates',
     },
-    { img: TbCube, link: '/labs', text: 'Virtual Labs' },
+    { img: TbCube, link: '/student/labs', text: 'Virtual Labs' },
     {
       img: isSmallScreen ? MdChat : TbCreditCard,
-      link: '/payments',
+      link: '/student/payments',
       text: isSmallScreen ? 'Communication' : 'Payments',
     },
-    { img: CgProfile, link: '/profile', text: 'Profile' },
-    { img: PiHeadsetBold, link: '/support', text: 'Help & Support' },
+    { img: CgProfile, link: '/student/profile', text: 'Profile' },
+    { img: PiHeadsetBold, link: '/student/support', text: 'Help & Support' },
   ];
 
   return (
     <>
       <div
-        className={`bg-cp-primary w-[220px] text-white shadow-md z-[10000] h-full fixed top-10 left-0 bottom-0 transform transition-transform duration-300 ${
-          isOpen ? 'translate-x-0' : '-translate-x-full'
-        } md:translate-x-0`}
+        className={`bg-cp-primary w-[220px] text-white shadow-md z-[10000] h-full fixed top-10 left-0 bottom-0 transform transition-transform duration-300 ${isOpen ? 'translate-x-0' : '-translate-x-full'
+          } md:translate-x-0`}
       >
         <div className="flex py-10 justify-center items-center gap-2 border-b-[0.8px] border-cp-secondary md:hidden pb-5">
           <Image
