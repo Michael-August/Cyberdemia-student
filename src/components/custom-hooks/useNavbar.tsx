a30b26ae0f155762473cb859f08f7acf40a0fc3c
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2024-06-08 08:02:33 +0100

    fix error

--- src/components/custom-hooks/useNavbar.tsx
+++ src/components/custom-hooks/useNavbar.tsx
@@ -1,11 +1,18 @@
 'use client';
+import { useState } from 'react';
 import { useLayoutContext } from '../../../context/LayoutContext';
 import DashboardNavbar from '../ui/DashboardNavbar';
 import WebNavbar from '../ui/WebNavbar';
 
 export const Navbar = () => {
   const { state } = useLayoutContext();
+
+  // Define state for the sidebar
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   if (state.navbarType === 'webNavbar') return <WebNavbar />;
-  if (state.navbarType === 'dashboardNavbar') return <DashboardNavbar />;
+  if (state.navbarType === 'dashboardNavbar') {
+    return <DashboardNavbar setSidebarOpen={setSidebarOpen} isSidebarOpen={isSidebarOpen} />;
+  }
   return null;
 };
