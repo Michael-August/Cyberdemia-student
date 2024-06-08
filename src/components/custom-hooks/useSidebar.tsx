6d0e00dbe40bd101e6a1541df30267ba6497dea1
christopherabraham8@gmail.com <christopherabraham8@gmail.com>
2024-06-08 08:06:45 +0100

    fix error

--- src/components/custom-hooks/useSidebar.tsx
+++ src/components/custom-hooks/useSidebar.tsx
@@ -1,20 +1,21 @@
-"use client";
-import { useState } from "react";
-import { useLayoutContext } from "../../../context/LayoutContext";
-import CourseSidebar from "../ui/CourseSidebar";
-import DashboardNavbar from "../ui/DashboardNavbar";
+'use client';
+import { useState } from 'react';
+
+import { useLayoutContext } from '../../../context/LayoutContext';
+import CourseSidebar from '../ui/CourseSidebar';
+import DashboardNavbar from '../ui/DashboardNavbar';
 
 export const Sidebar = () => {
   const { state } = useLayoutContext();
   const [isSidebarOpen, setSidebarOpen] = useState(false);
 
-  if (state.sidebarType === "defaultSidebar")
+  if (state.sidebarType === 'defaultSidebar')
     return (
       <DashboardNavbar
         setSidebarOpen={setSidebarOpen}
         isSidebarOpen={isSidebarOpen}
       />
     );
-  if (state.sidebarType === "courseSideBar") return <CourseSidebar />;
+  if (state.sidebarType === 'courseSideBar') return <CourseSidebar />;
   return null;
 };
