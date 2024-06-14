9f894da7874ddbe43f7445113ab1cbbf18bdddf8
Michael-August <michaelfrancis135@gmail.com>
2024-06-14 15:47:15 +0100

    Help and support page design done.

--- src/components/help-support/Faqs.tsx
+++ src/components/help-support/Faqs.tsx
@@ -1,30 +1,79 @@
+"use client"
+
 import { Link } from 'lucide-react';
 
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-} from '../ui/breadcrumb';
+import { useState } from 'react';
+import { IoChevronUp, IoChevronDown } from 'react-icons/io5';
+import { faqsArray } from '../../../utils/constants/faqs';
+import Breadcrumb from '../ui/breadcrumb';
+
+const breadcrumbs = [
+    {
+        url: '/student/support',
+        name: 'Help & Support'
+    },
+    {
+        url: '',
+        name: 'FAQs'
+    },
+]
 
 const Faqs = () => {
-  return (
-    <div>
-      <div className="breadcrumbs">
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem>
-              <BreadcrumbLink>
-                <Link href="/">Home</Link>
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            {/* ... */}
-          </BreadcrumbList>
-        </Breadcrumb>
-      </div>
-      <div className="faqs"></div>
-    </div>
-  );
+
+    const [faqs, setFaqs] = useState(faqsArray);
+
+    const expandFaq = (faqId: number) => {
+        const faqsUpdated = faqs.map((faq) => {
+        if (faq.id === faqId) {
+            faq.expanded = !faq.expanded;
+        }
+        return faq;
+        });
+        setFaqs(faqsUpdated);
+    };
+
+
+    return (
+        <div>
+        <div className="breadcrumbs">
+            <Breadcrumb breadcrumbsArray={breadcrumbs} />
+        </div>
+        <div className="faqs mt-8">
+            <div className="questions">
+            {faqs.map((faq: any) => (
+                <div
+                key={faq.id}
+                className="question md:py-8 py-5 border-t border-solid border-cp-secondary"
+                >
+                <div className="question-head flex items-center justify-between">
+                    <span className="text-base md:text-lg font-semibold text-[#000000]">
+                    {faq.question}
+                    </span>
+                    {faq.expanded ? (
+                    <IoChevronUp
+                        onClick={() => expandFaq(faq.id)}
+                        size={22}
+                        className="text-cp-secondary cursor-pointer"
+                    />
+                    ) : (
+                    <IoChevronDown
+                        onClick={() => expandFaq(faq.id)}
+                        size={22}
+                        className="text-cp-secondary cursor-pointer"
+                    />
+                    )}
+                </div>
+                <div className={`answer mt-8 ${faq.expanded ? 'expanded' : ''}`}>
+                    <span className="text-base md:text-lg font-normal text-[#000000B2]">
+                    {faq.answer}
+                    </span>
+                </div>
+                </div>
+            ))}
+            </div>
+        </div>
+        </div>
+    );
 };
 
 export default Faqs;
