29fceeb89b43b5fdee6d686521136162e165e742
Michael-August <michaelfrancis135@gmail.com>
2024-06-19 07:53:23 +0100

    Merge after pull

--- src/components/home/CustomCourse.tsx
+++ src/components/home/CustomCourse.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import React from 'react';
 import { GoArrowRight } from 'react-icons/go';
