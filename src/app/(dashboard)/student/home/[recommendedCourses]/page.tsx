01624490b47416118c05f4f99f35a1a6917f768e
omenvc <chimavalentine11@gmail.com>
2024-07-08 10:50:36 +0100

    ui updated

--- src/app/(dashboard)/student/home/[recommendedCourses]/page.tsx
+++ src/app/(dashboard)/student/home/[recommendedCourses]/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
-import RecomendedCourse from "@/components/home/RecomendedCourse";
-import { NavigationCrumbs } from "@/components/NavigationCrumbs";
+import RecomendedCourse from '@/components/home/RecomendedCourse';
+import { NavigationCrumbs } from '@/components/NavigationCrumbs';
 
-import { useLayoutContext } from "../../../../../../context/LayoutContext";
+import { useLayoutContext } from '../../../../../../context/LayoutContext';
 function RecomendedCourses() {
   const router = useRouter();
   const { dispatch } = useLayoutContext();
 
   useEffect(() => {
-    dispatch({ type: "SET_NAVBAR", navbarType: "dashboardNavbar" });
-    dispatch({ type: "SET_SIDEBAR", sidebarType: "defaultSidebar" });
+    dispatch({ type: 'SET_NAVBAR', navbarType: 'dashboardNavbar' });
+    dispatch({ type: 'SET_SIDEBAR', sidebarType: 'defaultSidebar' });
   }, [dispatch]);
 
   return (
@@ -24,52 +24,52 @@ function RecomendedCourses() {
         </span>
 
         <div
-          style={{ gap: "0px 20px" }}
+          style={{ gap: '0px 20px' }}
           className="md:grid overflow-x-auto md:w-max flex  md:grid-cols-3 "
         >
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/rrhh")}
+            handleClick={() => router.push('dhjfhjdfh/rrhh')}
             isCourseAvailable={false}
             isCourseComing={false}
           />
 
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/rg")}
+            handleClick={() => router.push('dhjfhjdfh/rg')}
             isCourseAvailable={false}
             isCourseComing={false}
           />
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/ff")}
+            handleClick={() => router.push('dhjfhjdfh/ff')}
             isCourseAvailable={false}
             isCourseComing={false}
           />
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/wws")}
+            handleClick={() => router.push('dhjfhjdfh/wws')}
             isCourseAvailable={false}
             isCourseComing={false}
           />
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/jks")}
+            handleClick={() => router.push('dhjfhjdfh/jks')}
             isCourseAvailable={false}
             isCourseComing={false}
           />
@@ -78,24 +78,24 @@ function RecomendedCourses() {
       <div className="flex flex-col gap-3 -mt-12">
         <span className="text-14px font-extrabold">Coming Soon</span>
         <div
-          style={{ gap: "0px 20px" }}
+          style={{ gap: '0px 20px' }}
           className="md:grid overflow-x-auto md:w-max flex  md:grid-cols-3"
         >
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("dhjfhjdfh/ksks")}
+            handleClick={() => router.push('dhjfhjdfh/ksks')}
             isCourseAvailable={false}
             isCourseComing={true}
           />
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card3.svg"}
+            image={'/images/card3.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("home/dhjfhjdfh/ksks")}
+            handleClick={() => router.push('home/dhjfhjdfh/ksks')}
             isCourseAvailable={false}
             isCourseComing={true}
           />
--- src/app/(dashboard)/student/home/page.tsx
+++ src/app/(dashboard)/student/home/page.tsx
@@ -1,18 +1,18 @@
-"use client";
-import { useRouter } from "next/navigation";
-import React, { useEffect } from "react";
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { useEffect } from 'react';
 
-import CustomCourse from "@/components/home/CustomCourse";
-import RecomendedCourse from "@/components/home/RecomendedCourse";
-import ResumeLearning from "@/components/home/ResumeLearning";
+import CustomCourse from '@/components/home/CustomCourse';
+import RecomendedCourse from '@/components/home/RecomendedCourse';
+import ResumeLearning from '@/components/home/ResumeLearning';
 
-import { useLayoutContext } from "../../../../../context/LayoutContext";
+import { useLayoutContext } from '../../../../../context/LayoutContext';
 const Home = () => {
   const { dispatch } = useLayoutContext();
 
   useEffect(() => {
-    dispatch({ type: "SET_NAVBAR", navbarType: "dashboardNavbar" });
-    dispatch({ type: "SET_SIDEBAR", sidebarType: "defaultSidebar" });
+    dispatch({ type: 'SET_NAVBAR', navbarType: 'dashboardNavbar' });
+    dispatch({ type: 'SET_SIDEBAR', sidebarType: 'defaultSidebar' });
   }, [dispatch]);
   const router = useRouter();
   return (
@@ -25,9 +25,9 @@ const Home = () => {
           <div className="w-[108px] h-[7px] bg-[#AC1D7E]"></div>
           <hr
             style={{
-              height: "1px",
-              backgroundColor: "#AC1D7E",
-              border: "none",
+              height: '1px',
+              backgroundColor: '#AC1D7E',
+              border: 'none',
             }}
           />
         </div>
@@ -36,7 +36,7 @@ const Home = () => {
       <ResumeLearning />
 
       <hr
-        style={{ height: "1px", backgroundColor: "#AC1D7E", border: "none" }}
+        style={{ height: '1px', backgroundColor: '#AC1D7E', border: 'none' }}
       />
       <div className="flex flex-col gap-3">
         <span className="text-[16px] font-extrabold">
@@ -51,19 +51,19 @@ const Home = () => {
         <div className="flex flex-col md:flex-row  gap-6">
           <RecomendedCourse
             availableCourses="7 Courses Available"
-            image={"/images/card1.svg"}
+            image={'/images/card1.svg'}
             title="Cyber Security Technical Training"
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("home/dhjfhjdfh")}
+            handleClick={() => router.push('home/dhjfhjdfh')}
             isCourseAvailable={true}
             isCourseComing={false}
           />
           <RecomendedCourse
             availableCourses="3 Courses Available"
-            image={"/images/card2.svg"}
+            image={'/images/card2.svg'}
             title="Cyber Security Awareness "
             body="Lorem ipsum dolor sit amet consectetur. Ut porttitor et viverra malesuada fringilla. Dictum vitae mi nunc a tellus. Faucibus ac id pellentesque interdum. Vestibulum convallis velit feugiat aliquam pellentesque etiam. In posuere purus aliquet dolor pretium eget dictum."
-            handleClick={() => router.push("home/shfdhf")}
+            handleClick={() => router.push('home/shfdhf')}
             isCourseAvailable={true}
             isCourseComing={false}
           />
