d54fbf18fc08e616a6e8723d9488dbbe70728436
Michael-August <michaelfrancis135@gmail.com>
2024-08-03 08:45:40 +0100

    Corrections on app done

--- src/components/certificates/index.tsx
+++ src/components/certificates/index.tsx
@@ -1,12 +1,8 @@
 'use client';
 
-import Image from 'next/image';
-import { IoArrowForward } from 'react-icons/io5';
 
-import { Button } from '../button';
-import { Tab, Tabs } from '../ui/Tab';
 import Completed from '../courses/tabs/Completed';
-import InProgess from '../courses/tabs/InProgess';
+import { Tab, Tabs } from '../ui/Tab';
 
 const Certificates = () => {
   return (
