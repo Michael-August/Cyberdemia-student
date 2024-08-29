02c3086805c8e3f6b1ba14fed7f536849edbe415
Chisomchima <chimachisom360@gmail.com>
2024-08-29 20:32:11 +0100

    request update

--- src/components/auth/Signin-form.tsx
+++ src/components/auth/Signin-form.tsx
@@ -6,7 +6,7 @@ import { SubmitHandler, useForm } from 'react-hook-form';
 import { AiFillEye, AiFillEyeInvisible } from 'react-icons/ai';
 import { toast } from 'react-toastify';
 
-import { useRequest } from '@/hooks/request';
+import { request } from '@/hooks/request';
 
 import { Input } from '../inputs';
 import { Label } from '../label';
@@ -49,7 +49,7 @@ const SigninForm: React.FC = () => {
           method: 'get',
           url: '/get-profile',
         };
-        const profileRes = await useRequest(config);
+        const profileRes = await request(config);
 
         if (profileRes && profileRes?.data) {
           const { data: profileData } = profileRes;
